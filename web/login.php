58f3de61d7ea5c2ce0aca767f394727a5f707aa4
Tavi <taving40@gmail.com>
2020-11-16 15:15:52 +0200

    modified:   web/login.php

--- web/login.php
+++ web/login.php
@@ -6,13 +6,13 @@ asdfasdfasdf
 <?php
 
 echo 1;
-include_once '/api/user/read_one.php';
+//include_once '/api/user/read_one.php';
 
 $data = json_encode($_POST);
 
-if(read_one($data))
-    echo "Login Succesful!";
-else echo "Login failed.";
+// if(read_one($data))
+//     echo "Login Succesful!";
+// else echo "Login failed.";
 
 ?>
 
