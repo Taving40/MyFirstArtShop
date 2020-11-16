0a2770cdc2c336a6ef309abce82c6ab34351cb12
Tavi <taving40@gmail.com>
2020-11-16 16:30:10 +0200

    some changes

--- web/api/user/read_one.php
+++ web/api/user/read_one.php
@@ -5,8 +5,8 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Credentials: true");
 header('Content-Type: application/json');
   
-include_once '../config/database.php';
-include_once '../objects/user.php';
+include_once dirname(__DIR__).'/config/database.php';
+include_once dirname(__DIR__).'/objects/user.php';
   
 function read_one($data){
 
@@ -14,19 +14,28 @@ function read_one($data){
     $db = $database->getConnection();
     $user = new User($db);
 
-    //$data = json_decode(file_get_contents("php://input"));
+    $data = json_decode($data);
 
     $user->email = $data->email;
 
     $user->readOne();
-    
-    if($user->email){
 
-        $user_arr = array(
-            "email" => $user->email,
-            "name" => $user->name,
-            "password" => $user->password
-        );
+    //$data->email = $user->email;
+
+    echo "\n\n\n";
+    echo ($user->email);
+    echo ($user->password);
+    echo ($user->name);
+    echo "\n\n\n";
+    //echo ($data->name == $user->name);
+    
+    if($user->email && $user->password == $data->password){
+
+        // $user_arr = array(
+        //     "email" => $user->email,
+        //     "name" => $user->name,
+        //     "password" => $user->password
+        // );
     
         //200 OK
         //http_response_code(200);
--- web/login.php
+++ web/login.php
@@ -1,20 +1,17 @@
 <html>
 <body>
 
-asdfasdfasdf
-
 <?php
 
-echo 1;
-//include_once '/api/user/read_one.php';
+include_once __DIR__ . '/api/user/read_one.php';
 
 $data = json_encode($_POST);
 
-echo $data;
+if(read_one($data))
+    echo "Login Succesful!";
+else echo "Login failed.";
 
-// if(read_one($data))
-//     echo "Login Succesful!";
-// else echo "Login failed.";
+echo $data;
 
 ?>
 
