9bbd3456cca2a9beb9f6560c46bc70a33f0bef03
Tavi <taving40@gmail.com>
2021-01-02 17:34:55 +0200

    fixing phpmailer

--- web/api/config/mail.php
+++ web/api/config/mail.php
@@ -13,13 +13,17 @@ use PHPMailer\PHPMailer\PHPMailer;
 
 function mailer($to, $from, $from_name, $subject, $body) { 
 	$mail = new PHPMailer();  
+
 	$mail->IsSMTP(); 
+
+	$mail->SMTPDebug  = 0;    
 	$mail->SMTPAuth = true;  
 	$mail->SMTPSecure = 'ssl'; 
 	$mail->Host = 'smtp.gmail.com';
-	$mail->Port = 465; 
+	$mail->Port = 25; 
 	$mail->Username = 'myfirstartshop@gmail.com';  
-	$mail->Password = 'pandacactusturtle3';           
+	$mail->Password = 'pandacactusturtle3';   
+
 	$mail->SetFrom($from, $from_name);
 	$mail->Subject = $subject;
 	$mail->Body = $body;
