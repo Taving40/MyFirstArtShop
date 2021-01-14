62796c702046755f6240b45c70dbea35dc07bfa1
Tavi <taving40@gmail.com>
2021-01-14 15:39:33 +0200

    ...

--- MySql generate script.sql
+++ MySql generate script.sql
@@ -1,3 +1,5 @@
+/*TABLE CREATION SCRIPT*/
+
 CREATE TABLE `users` (
   `email` varchar(255) UNIQUE PRIMARY KEY NOT NULL,
   `name` varchar(255) NOT NULL,
@@ -11,14 +13,14 @@ CREATE TABLE `products` (
   `price` int NOT NULL,
   `description` varchar(255) NOT NULL,
   `quantity` int NOT NULL,
-  `size` ENUM ('small', 'medium', 'big') NOT NULL,
+  `size` ENUM ('small', 'medium', 'large') NOT NULL,
   `type` ENUM ('painting', 'sculpture', 'craft') NOT NULL
 )Engine=InnoDB;
 
 CREATE TABLE `stores` (
   `id` int UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `admin_email` varchar(255) NOT NULL,
-  `store_nume` varchar(255) NOT NULL,
+  `store_nume` varchar(255) UNIQUE NOT NULL,
   `score` float4 NOT NULL DEFAULT 0,
   `nr_tranzactii` int NOT NULL DEFAULT 0
 )Engine=InnoDB;
@@ -34,7 +36,7 @@ CREATE TABLE `order` (
   `id` int UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `user_email` varchar(255) NOT NULL,
   `status` ENUM ('in_transit', 'failed', 'succesful') NOT NULL,
-  `responsabil_id` int NOT NULL,
+  `responsabil_id` int NOT NULL
 )Engine=InnoDB;
 
 CREATE TABLE `order_items` (
@@ -46,27 +48,32 @@ CREATE TABLE `order_items` (
 
 CREATE TABLE `reviews` (
   `id` int UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `order_id` int NOT NULL,
   `score` int NOT NULL,
   `user_email` varchar(255) NOT NULL,
   `store_id` int NOT NULL
 )Engine=InnoDB;
 
-ALTER TABLE `order_items` ADD FOREIGN KEY (`order_id`) REFERENCES `order` (`id`);
+ALTER TABLE `reviews` ADD FOREIGN KEY (`order_id`) REFERENCES `order` (`id`) ON DELETE CASCADE;
 
-ALTER TABLE `order_items` ADD FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
+ALTER TABLE `order_items` ADD FOREIGN KEY (`order_id`) REFERENCES `order` (`id`) ON DELETE CASCADE;
 
-ALTER TABLE `products` ADD FOREIGN KEY (`store_id`) REFERENCES `stores` (`id`);
+ALTER TABLE `order_items` ADD FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE;
 
-ALTER TABLE `stores` ADD FOREIGN KEY (`admin_email`) REFERENCES `users` (`email`);
+ALTER TABLE `products` ADD FOREIGN KEY (`store_id`) REFERENCES `stores` (`id`) ON DELETE CASCADE;
 
-ALTER TABLE `carts` ADD FOREIGN KEY (`user_email`) REFERENCES `users` (`email`);
+ALTER TABLE `stores` ADD FOREIGN KEY (`admin_email`) REFERENCES `users` (`email`) ON DELETE CASCADE;
 
-ALTER TABLE `carts` ADD FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
+ALTER TABLE `carts` ADD FOREIGN KEY (`user_email`) REFERENCES `users` (`email`) ON DELETE CASCADE;
 
-ALTER TABLE `order` ADD FOREIGN KEY (`user_email`) REFERENCES `users` (`email`);
+ALTER TABLE `carts` ADD FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE;
 
-ALTER TABLE `order` ADD FOREIGN KEY (`responsabil_id`) REFERENCES `stores` (`id`);
+ALTER TABLE `order` ADD FOREIGN KEY (`user_email`) REFERENCES `users` (`email`) ON DELETE CASCADE;
 
-ALTER TABLE `reviews` ADD FOREIGN KEY (`store_id`) REFERENCES `stores` (`id`);
+ALTER TABLE `order` ADD FOREIGN KEY (`responsabil_id`) REFERENCES `stores` (`id`) ON DELETE CASCADE;
 
-ALTER TABLE `reviews` ADD FOREIGN KEY (`user_email`) REFERENCES `users` (`email`);
+ALTER TABLE `reviews` ADD FOREIGN KEY (`store_id`) REFERENCES `stores` (`id`) ON DELETE CASCADE;
+
+ALTER TABLE `reviews` ADD FOREIGN KEY (`user_email`) REFERENCES `users` (`email`) ON DELETE CASCADE;
+
+
